b9645ae7a2ec518227f256d002190401eee8e1a2
sourcedevil <wizard@sourcedevil.com>
2020-05-26 06:41:39 -0500

    projecto actualizado

--- src/main/java/com/redhat/developers/msa/ola/OlaController.java
+++ src/main/java/com/redhat/developers/msa/ola/OlaController.java
@@ -16,55 +16,30 @@
  */
 package com.redhat.developers.msa.ola;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-
-import org.keycloak.KeycloakPrincipal;
-import org.keycloak.adapters.RefreshableKeycloakSecurityContext;
-import org.keycloak.representations.AccessToken;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
 
-import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/api")
 public class OlaController {
 
 
-    @CrossOrigin
+    @Value("${com.redhat.developers.msa.ola.loc:true}")
+    private boolean loc;
+
+
     @RequestMapping(method = RequestMethod.GET, value = "/ola", produces = "text/plain")
-    @ApiOperation("Returns the greeting in Portuguese")
     public String ola() {
-        String hostname = System.getenv().getOrDefault("HOSTNAME", "Unknown");
-        return String.format("Olá de %s", hostname);
-    }
-
-  
-
-    @CrossOrigin
-    @RequestMapping(method = RequestMethod.GET, value = "/ola-secured", produces = "text/plain")
-    @ApiOperation("Returns a message that is only available for authenticated users")
-    public String olaSecured(KeycloakPrincipal<RefreshableKeycloakSecurityContext> principal) {
-        AccessToken token = principal.getKeycloakSecurityContext().getToken();
-        return "This is a Secured resource. You are logged as " + token.getName();
-    }
-
-    @CrossOrigin
-    @RequestMapping(method = RequestMethod.GET, value = "/logout", produces = "text/plain")
-    @ApiOperation("Logout")
-    public String logout() throws ServletException {
-        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
-        request.logout();
-        return "Logged out";
+    	if (loc) {
+            String hostname = System.getenv().getOrDefault("HOSTNAME", "Unknown");
+            return String.format("Olá de %s", hostname);
+    	}else {
+    		return "ola";
+    	}
+    	
     }
 
   
